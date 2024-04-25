4cd9cd305b5f486b28a7173362f36a9bc882f5c5
Brad Weyand <memusicman@outlook.com>
2024-04-24 23:32:35 -0400

    dummy backend finished

--- SpotifyGameBackend/app.mjs
+++ SpotifyGameBackend/app.mjs
@@ -1,27 +1,66 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+
+const CLIENT_ID = "acaeb65eed5644c798b3ae8a3ef7cde2";
+const CLIENT_SECRET = "c8668ba1c0744c0c8857089d1d6bf6d2";
+let token = 0;
+
+async function postData(url = "", data = {}) {    // Default options are marked with *
+    const response = await fetch(url, {
+        method: "POST", 
+        mode: "cors", 
+        cache: "no-cache", 
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded',
+        },
+        referrerPolicy: "no-referrer", 
+        body: "grant_type=client_credentials&client_id=acaeb65eed5644c798b3ae8a3ef7cde2&client_secret=c8668ba1c0744c0c8857089d1d6bf6d2"
+    });
+    return response.json(); 
+}
+  
+postData('https://accounts.spotify.com/api/token').then((data) => {
+    console.log(data); 
+    token = data.access_token
+});
+
 const app = express();
 
 const port = 3000;
 
 app.use(bodyParser.json());
 
+//get info about a user
 app.get('/user/:name', (req, res) => {
-    res.status(500).send("still needs to be implemented");
+    let user = {username:"test", scores: [{song: {name: "song1", id:12345}, points: [1,2,3,4]}]}
+    res.json(user);
 });
 
+//create a new user
 app.post('/user', (req, res) => {
-    res.status(500).send("still needs to be implemented");
+    let user = {username:"test", scores: [{song: {name: "song1", id:12345}, points: [1,2,3,4]}]}
+    res.json(user);
 });
 
+//update a user
 app.put('/user/:name', (req, res) => {
-    res.status(500).send("still needs to be implemented");
+    let user = {username:"test", scores: [{song: {name: "song1", id:12345}, points: [1,2,3,4]}]}
+    res.json(user);
 });
 
+//get a list of artist ids in the form {name: <name>, id:<id>}
 app.get('/artist/:name', (req, res) => {
-    res.status(500).send("still needs to be implemented");
+    let artists = [{name: artist1, id:12345}, {name:artist2, id:54321}];
+    res.json(artist);
 });
+
+//get a list of mp3s for an artist id
+app.get('/artist/songs/:id', (req, res) => {
+    let links = ['https://p.scdn.co/mp3-preview/0a69f787920bde0a0e1d17af762695ad3cc9d99b?cid=acaeb65eed5644c798b3ae8a3ef7cde2'];
+    res.json(links);
+});
+
 
 app.get('/songs/:id', (req, res) => {
     res.status(500).send("still needs to be implemented");

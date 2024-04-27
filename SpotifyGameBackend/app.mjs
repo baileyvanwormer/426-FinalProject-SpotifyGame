a7392d71d8ccc463900ee33a6c08f1cf96994238
Brad Weyand <memusicman@outlook.com>
2024-04-27 13:41:13 -0400

    refactored and fixed errors

--- SpotifyGameBackend/app.mjs
+++ SpotifyGameBackend/app.mjs
@@ -6,7 +6,7 @@ const CLIENT_ID = "acaeb65eed5644c798b3ae8a3ef7cde2";
 const CLIENT_SECRET = "c8668ba1c0744c0c8857089d1d6bf6d2";
 let token = 0;
 
-async function postData(url = "", data = {}) {    // Default options are marked with *
+async function postData(url = "", data = {}) {   
     const response = await fetch(url, {
         method: "POST", 
         mode: "cors", 
@@ -15,7 +15,7 @@ async function postData(url = "", data = {}) {    // Default options are marked 
             "Content-Type": 'application/x-www-form-urlencoded',
         },
         referrerPolicy: "no-referrer", 
-        body: "grant_type=client_credentials&client_id=acaeb65eed5644c798b3ae8a3ef7cde2&client_secret=c8668ba1c0744c0c8857089d1d6bf6d2"
+        body: "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET
     });
     return response.json(); 
 }
@@ -52,12 +52,12 @@ app.put('/user/:name', (req, res) => {
 //get a list of artist ids in the form {name: <name>, id:<id>}
 app.get('/artist/:name', (req, res) => {
     let artists = [{name: artist1, id:12345}, {name:artist2, id:54321}];
-    res.json(artist);
+    res.json(artists);
 });
 
 //get a list of mp3s for an artist id
 app.get('/artist/songs/:id', (req, res) => {
-    let links = ['https://p.scdn.co/mp3-preview/0a69f787920bde0a0e1d17af762695ad3cc9d99b?cid=acaeb65eed5644c798b3ae8a3ef7cde2'];
+    let links = [{name: "testsong1", link: 'https://p.scdn.co/mp3-preview/0a69f787920bde0a0e1d17af762695ad3cc9d99b?cid=acaeb65eed5644c798b3ae8a3ef7cde2'}];
     res.json(links);
 });
 

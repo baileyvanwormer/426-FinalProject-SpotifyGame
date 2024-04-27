203950852cf227f2378424e84f7300b6feea0edc
Bailey Van Wormer <baileyvanwormer@baileys-mbp.wireless-1x.unc.edu>
2024-04-27 16:26:42 -0400

    add audio player in game-view

--- SpotifyGame/src/app/game-view/game-view.component.ts
+++ SpotifyGame/src/app/game-view/game-view.component.ts
@@ -8,13 +8,30 @@ import { GameService } from '../game.service';
 })
 export class GameViewComponent {
 
+  private gameState: string = "initial";
+
   constructor(private service: GameService) {}
 
-  public generateSong() {
+  public generateSong(artistName: string) {
     // logic to call service method which calls previews api and generates song goes here
+    this.service.setArtistName(artistName);
+    this.setGameState('playing');
+  }
+
+  public getArtistName() {
+    return this.service.getArtistName();
+  }
+
+  public getGameState() {
+    return this.gameState;
   }
 
   public setArtistName(artistName: string) {
     this.service.setArtistName(artistName);
   }
+
+  public setGameState(gameState: string) {
+    this.gameState = gameState;
+  }
+
 }
